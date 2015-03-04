e0da970d739b057f850eaf60d0b51cb3c4ede90b
vijay <vijay.iyengar713@gmail.com>
2015-03-03 23:14:00 -0500

    Aggregating both bigrams and unigrams in one step

--- src/Aggregate.java
+++ src/Aggregate.java
@@ -9,13 +9,13 @@ import java.util.*;
  * Created by vijay on 3/3/15.
  */
 public class Aggregate {
-    private static final String bigramFormat = "%s\tBxy=%d,Cxy=%d\n";
+    private static final String bigramFormat = "%s\t\n";
     private static final String unigramFormat = "%s\tBx=%d,Cx=%d\n";
     private static final String FOREGROUND_DECADE = "1960";
     private static String[] STOP_WORD_ARRAY = {"i", "the", "to", "and", "a", "an", "of", "it", "you", "that", "in", "my", "is", "was", "for"};
     private static final Set<String> STOP_WORDS = new HashSet<String>(Arrays.asList(STOP_WORD_ARRAY));
 
-    static boolean containsStopWords(String phrase) {
+    private static boolean containsStopWords(String phrase) {
         String[] tokens = phrase.split(" ");
         for (String token : tokens) {
             if (STOP_WORDS.contains(token))
@@ -24,16 +24,17 @@ public class Aggregate {
         return false;
     }
 
-    public static class BigramMapper extends MapReduceBase implements
+    public static class AggregateMapper extends MapReduceBase implements
             Mapper<LongWritable, Text, Text, Text> {
-        private static final String FOREGROUND_DECADE = "1960";
 
-        int foreCount = 0;
-        int backCount = 0;
-        long uniqueFgBigrams = 0;
-        long totalFgBigrams = 0;
-        long uniqueBgBigrams = 0;
-        long totalBgBigrams = 0;
+
+        private Text formatCount(boolean isForeground, boolean isBigram, int count) {
+            if (isBigram) {
+                return new Text(String.format(isForeground ? "Cxy=%d" : "Bxy=%d", count));
+            } else {
+                return new Text(String.format(isForeground ? "Cx=%d" : "Bx=%d", count));
+            }
+        }
 
         @Override
         public void map(LongWritable longWritable, Text value, OutputCollector<Text,
@@ -47,77 +48,25 @@ public class Aggregate {
 
             // Output the appropriate foreground or background count string
             if (!containsStopWords(phrase)) {
+                boolean isBigram = phrase.contains(" ");
                 boolean isForeground = decade.equals(FOREGROUND_DECADE);
-                Text output = new Text(String.format(isForeground ? "Cxy=%d" : "Bxy=%d", count));
 
-                outputCollector.collect(new Text(phrase), output);
-            }
-        }
-/*
-            // New phrase; flush the counts of the previous phrase
-            if (!curPhrase.equals(prevPhrase)) {
-                if (prevPhrase == null) {
-                    prevPhrase = curPhrase;
-                } else {
-                    counts = new Text(createCountString(true, prevPhrase, backCount, foreCount));
-                    outputCollector.collect(new Text(prevPhrase), counts);
-                    // Update total counts for probability computation and smoothing
-                    if (foreCount > 0) {
-                        uniqueFgBigrams++;
-                        totalFgBigrams += foreCount;
-                    }
-                    if (backCount > 0) {
-                        uniqueBgBigrams++;
-                        totalBgBigrams += foreCount;
-                    }
-                     if (foreCount > 0) {
-                            uniqueFgUnigrams++;
-                            totalFgUnigrams += foreCount;
-                        }
-                    }
-
-                    // Reset local variables
-                    prevPhrase = curPhrase;
-                    foreCount = 0;
-                    backCount = 0;
-                }
-            }
-*/
-    }
-
-
-    public static class UnigramMapper extends MapReduceBase implements
-            Mapper<LongWritable, Text, Text, Text> {
-
-        String prevPhrase = null, curPhrase = null;
-        int foreCount = 0;
-        int backCount = 0;
-        long uniqueFgUnigrams = 0;
-        long totalFgUnigrams = 0;
-
-        @Override
-        public void map(LongWritable longWritable, Text value, OutputCollector<Text,
-                Text> outputCollector, Reporter reporter) throws IOException {
-
-            // Parse the line
-            String[] tokens = value.toString().split("\t");
-            String phrase = tokens[0];
-            String decade = tokens[1];
-            int count = Integer.valueOf(tokens[2]);
-
-            // Output the appropriate foreground or background count string
-            if (!containsStopWords(phrase)) {
-                boolean isForeground = decade.equals(FOREGROUND_DECADE);
-                Text output = new Text(String.format(isForeground ? "Cx=%d" : "Bx=%d", count));
-
-                outputCollector.collect(new Text(phrase), output);
+                outputCollector.collect(new Text(phrase), formatCount(isForeground, isBigram, count));
             }
         }
     }
 
-
-    public static class BigramReducer extends MapReduceBase implements
+    public static class AggregateReducer extends MapReduceBase implements
             Reducer<Text, Text, Text, Text> {
+
+        private Text formatOutput(boolean isBigram, int bgCount, int fgCount) {
+            if (isBigram) {
+                return new Text(String.format("Bxy=%d,Cxy=%d", bgCount, fgCount));
+            } else {
+                return new Text(String.format("Bx=%d,Cx=%d", bgCount, fgCount));
+            }
+        }
+
         @Override
         public void reduce(Text key, Iterator<Text> values,
                            OutputCollector<Text, Text> context,
@@ -125,46 +74,28 @@ public class Aggregate {
              /* Values are counts of form: Bxy=%d, Cxy=%d. Need to collect and reduce*/
             int bgCount = 0;
             int fgCount = 0;
+            boolean isBigram = key.toString().contains(" ");
             String countType;
             int count;
             while (values.hasNext()) {
                 String[] tokens = values.next().toString().split("=");
                 countType = tokens[0];
                 count = Integer.valueOf(tokens[1]);
-                if (countType.equals("Bxy"))
-                    bgCount += count;
-                else if (countType.equals("Cxy"))
-                    fgCount += count;
+
+                if (isBigram) {
+                    if (countType.equals("Bxy"))
+                        bgCount += count;
+                    else if (countType.equals("Cxy"))
+                        fgCount += count;
+                } else {
+                    if (countType.equals("Bx"))
+                        bgCount += count;
+                    else if (countType.equals("Cx"))
+                        fgCount += count;
+                }
             }
+            context.collect(key, formatOutput(isBigram, bgCount, fgCount));
 
-            context.collect(key, new Text(String.format("Bxy=%d,Cxy=%d", bgCount, fgCount)));
-        }
-    }
-
-    public static class UnigramReducer extends MapReduceBase implements
-            Reducer<Text, Text, Text, Text> {
-        @Override
-        public void reduce(Text key, Iterator<Text> values,
-                           OutputCollector<Text, Text> context,
-                           Reporter reporter) throws IOException {
-             /* Values are counts of form: Bxy=%d, Cxy=%d. Need to collect and reduce*/
-            int bgCount = 0;
-            int fgCount = 0;
-            String countType;
-            int count;
-            while (values.hasNext()) {
-                String s = values.next().toString();
-                System.out.println("sac: " + key.toString() + " " + s);
-                String[] tokens = s.split("=");
-                countType = tokens[0];
-                count = Integer.valueOf(tokens[1]);
-                if (countType.equals("Bx"))
-                    bgCount += count;
-                else if (countType.equals("Cx"))
-                    fgCount += count;
-            }
-
-            context.collect(key, new Text(String.format("Bx=%d,Cx=%d", bgCount, fgCount)));
         }
     }
 
--- src/run.java
+++ src/run.java
@@ -12,54 +12,35 @@ import java.io.IOException;
  */
 public class run {
 
-
-    public static void aggregateUnigram(Path unigramInput, Path unigramAggregated) throws IOException{
+    public static void aggregate(Path unigramInput, Path bigramInput, Path aggregatedOut) throws IOException{
         JobConf conf = new JobConf(Aggregate.class);
-        conf.setJobName("Aggregate Unigram");
+        conf.setJobName("Aggregate");
 
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(Text.class);
 
-        conf.setMapperClass(Aggregate.UnigramMapper.class);
-        conf.setReducerClass(Aggregate.UnigramReducer.class);
+        conf.setMapperClass(Aggregate.AggregateMapper.class);
+        conf.setReducerClass(Aggregate.AggregateReducer.class);
 
-        FileInputFormat.setInputPaths(conf, unigramInput);
-        FileOutputFormat.setOutputPath(conf, unigramAggregated);
-
-        JobClient.runJob(conf);
-    }
-
-    public static void aggregateBigram(Path bigramInput, Path bigramAggregated) throws IOException{
-        JobConf conf = new JobConf(Aggregate.class);
-        conf.setJobName("Aggregate Bigram");
-
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(Text.class);
-
-        conf.setMapperClass(Aggregate.BigramMapper.class);
-        conf.setReducerClass(Aggregate.BigramReducer.class);
-
-        FileInputFormat.setInputPaths(conf, bigramInput);
-        FileOutputFormat.setOutputPath(conf, bigramAggregated);
+        FileInputFormat.setInputPaths(conf, unigramInput, bigramInput);
+        FileOutputFormat.setOutputPath(conf, aggregatedOut);
 
         JobClient.runJob(conf);
     }
-
-
 
 
     public static void main(String[] args) throws Exception {
         Path unigramInput = new Path(args[0]);
         Path bigramInput = new Path(args[1]);
-        Path unigramProcessedTmp = new Path(args[2] + "/unigram_processed");
-        Path bigramProcessedTmp = new Path(args[2] + "/bigram_processed");
+        Path aggregatedTmp = new Path(args[2]);
         Path sizeCountTmp = new Path(args[3]);
         //Path unigramMessageTmp = new Path(args[4]);
         //Path output = new Path(args[5]);
         //int numReducers = Integer.valueOf(args[2]);
 
-        aggregateUnigram(unigramInput, unigramProcessedTmp);
-        aggregateBigram(bigramInput, bigramProcessedTmp);
+        aggregate(unigramInput, bigramInput, aggregatedTmp);
+        //aggregateUnigram(unigramInput, unigramProcessedTmp);
+        //aggregateBigram(bigramInput, bigramProcessedTmp);
 
         /*JobConf conf = new JobConf(Aggregate.class);
 
